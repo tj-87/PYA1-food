ebd43f7cc499a2645805bd9a6de30b58f18700c9
Thiago <thiago380tt@gmail.com>
2025-08-14 18:52:56 -0300

    'feat: installing expo google fonts'

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
+import { useFonts, Sen_700Bold, Sen_400Regular } from '@expo-google-fonts/sen';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+    let [fontsLoaded] = useFonts({
+      Sen_700Bold,
+      Sen_400Regular
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text  style={{ fontFamily: 'Sen_400Regular', fontSize: 20 }}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
