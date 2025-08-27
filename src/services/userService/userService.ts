f2142c31bba8744f81061989cf3322fdf0e66b55
Thiago <thiago380tt@gmail.com>
2025-08-27 19:54:05 -0300

    'feat: data persistence with AsyncStorage'

---
+++ src/services/userService/userService.ts
@@ -0,0 +1,28 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { USER } from "./userServiceConfig";
+import { userServiceProps } from "./userService.types";
+
+export async function saveUserName( name: userServiceProps  ){
+    try {
+    await AsyncStorage.setItem(USER, JSON.stringify(name));
+  } catch (error) {
+    console.error("Erro ao salvar usuário:", error);
+    throw error; 
+  }
+}
+
+
+export async function getUserName() {
+    try {
+    const storage = await AsyncStorage.getItem(USER);
+
+    const user: userServiceProps = storage ? JSON.parse(storage) : {} as userServiceProps;
+
+    console.log(user);
+    return user;
+  } catch (error) {
+    console.error("Erro ao buscar usuário:", error);
+    return {} as userServiceProps;
+  }
+}
